b8117296070e2fc305da6fdef069c3cbf41504d2
wbritten <arribas.daniel2@gmail.com>
2014-10-09 19:36:15 +0200

    pkm to flg

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var limit = {
 	min: 1, max: 195
 };
-var pkmElim = new Array();
-var pkmLike = new Array();
-var pkmSeen = new Array();
+var flgElim = new Array();
+var flgLike = new Array();
+var flgSeen = new Array();
 
 function startG() {
 	var _tmp = document.getElementsByTagName('select')[0];
 	_tmp.setAttribute('disabled', 'disabled');
-	document.getElementById('newPkm').setAttribute('disabled', 'disabled');
+	document.getElementById('newflg').setAttribute('disabled', 'disabled');
 	document.getElementById('settings').className = 'disabled';
 	document.getElementById('skip').removeAttribute('disabled');
 
 	//make sure the gen used will be what is selected
-	pkmElim = new Array();
-	pkmLike.length = 0;;
+	flgElim = new Array();
+	flgLike.length = 0;;
 	updateGen(_tmp.value, true);
 	reroll(0);
 }
@@ -24,13 +24,13 @@ function updateGen(value, start) {
 					[1, 195]];
 	for(i = 0; i < _limits.length; i++) {
 		if(i == value) {
-			limit.min = document.getElementById('newPkm').checked ? _limits[i][0] : 1;
+			limit.min = document.getElementById('newflg').checked ? _limits[i][0] : 1;
 			limit.max = _limits[i][1];
 			document.getElementsByTagName('span')[1].innerHTML = ''+ (limit.max - limit.min + 1);
 			if(!start)
 				break;
 			for(j = limit.min; j <= limit.max; j++) {
-				pkmLike.push(j);
+				flgLike.push(j);
 			}
 			break;
 		}
@@ -38,8 +38,8 @@ function updateGen(value, start) {
 }
 
 function reroll(fav) {
-	//console.log(pkmElim.length + '\t' + pkmLike.length);
-	if(pkmElim.length >= limit.max - limit.min) {
+	//console.log(flgElim.length + '\t' + flgLike.length);
+	if(flgElim.length >= limit.max - limit.min) {
 		return;
 	}
 	var _tmp = [document.getElementById('choice').getElementsByTagName('img')[0],
@@ -54,40 +54,40 @@ function reroll(fav) {
 			_tmp[1].setAttribute('onclick', 'reroll(2)');
 			break;
 		case 1:
-			pkmElim.push(_tmp[3]);
-			pkmSeen.push(_tmp[2]);
+			flgElim.push(_tmp[3]);
+			flgSeen.push(_tmp[2]);
 			break;
 		case 2:
-			pkmElim.push(_tmp[2]);
-			pkmSeen.push(_tmp[3]);
+			flgElim.push(_tmp[2]);
+			flgSeen.push(_tmp[3]);
 			break;
 		case 3: //skipped was press. don't eliminate.
-			pkmLike.push(_tmp[2]);
-			pkmLike.push(_tmp[3]);
+			flgLike.push(_tmp[2]);
+			flgLike.push(_tmp[3]);
 	}
 
 	//update top 9 image src
-	console.log(limit.max - limit.min - pkmElim.length);
-	if(limit.max - limit.min - pkmElim.length <= 8 && fav != 3 ) {
+	console.log(limit.max - limit.min - flgElim.length);
+	if(limit.max - limit.min - flgElim.length <= 8 && fav != 3 ) {
 		var x = document.getElementsByClassName('fav');
-		x[limit.max - limit.min - pkmElim.length].src = 'images/' + pkmElim[pkmElim.length -1] + '.png';
+		x[limit.max - limit.min - flgElim.length].src = 'images/' + flgElim[flgElim.length -1] + '.png';
 	}
 
 	//update choice image src
-	_tmp[0].src = 'images/' + _newPkm() + '.png';
-	if(pkmLike.length == 0 && pkmSeen.length== 0)
+	_tmp[0].src = 'images/' + _newflg() + '.png';
+	if(flgLike.length == 0 && flgSeen.length== 0)
 		_tmp[1].src = _tmp[0].src;
 	else 
-		_tmp[1].src = 'images/' + _newPkm() + '.png';
+		_tmp[1].src = 'images/' + _newflg() + '.png';
 
 	//update eliminated text
-	document.getElementsByTagName('span')[0].innerHTML = ''+ pkmElim.length;
+	document.getElementsByTagName('span')[0].innerHTML = ''+ flgElim.length;
 } 
 
-function _newPkm() {
-	if(pkmLike.length <= 0) {
-		pkmLike = pkmLike.concat(pkmSeen);
-		pkmSeen.length = 0;
+function _newflg() {
+	if(flgLike.length <= 0) {
+		flgLike = flgLike.concat(flgSeen);
+		flgSeen.length = 0;
 	}
-	return pkmLike.splice(Math.floor(Math.random() * pkmLike.length),1);
+	return flgLike.splice(Math.floor(Math.random() * flgLike.length),1);
 }
